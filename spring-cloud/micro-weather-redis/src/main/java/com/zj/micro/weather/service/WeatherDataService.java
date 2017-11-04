43afb4eb57afa46cb55ebe28967d5c4f31d63818
zhangjj <zhangjj@sinoufc.com>
2017-11-04 19:02:52 +0800

    修改目录结构

--- spring-cloud/micro-weather-redis/src/main/java/com/zj/micro/weather/service/WeatherDataService.java
+++
@@ -1,9 +0,0 @@
-package com.zj.micro.weather.service;
-
-import com.zj.micro.weather.vo.WeatherResponse;
-
-public interface WeatherDataService {
-	WeatherResponse getDataByCityId(String cityId);
-
-	WeatherResponse getDataByCityName(String cityName);
-}
